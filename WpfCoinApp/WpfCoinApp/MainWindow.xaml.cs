f29e3a995b3ca49579d527709ca121a25fd3f530
Влад Білошицький <vladbilosh01@gmail.com>
2025-06-02 12:16:30 +0300

    DisplayCoinInfo

---
+++ WpfCoinApp/WpfCoinApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using WpfCoinApp.Models;
+using System.Net;
+
+namespace WpfCoinApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string APIkey = "9937f378074076df75f56501936268a2a8efc25d656413055a936ca72f06dcc3";
+
+        private void SerchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GetCoinInfo();
+        }
+
+        void GetCoinInfo()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string url = string.Format("https://rest.coincap.io/v3/assets?apiKey={0}&appid={1}", TBCoin.Text, APIkey);
+                var json = client.DownloadString(url);
+                Coininfo.Root coinInfo = JsonConvert.DeserializeObject<Coininfo.Root>(json);
+
+                CoinName_Copy.DataContext = coinInfo.data.name;
+                CoinPrice_Copy2.DataContext = coinInfo.data.priceUsd;
+            }
+        }
+
+    }
+}
---
+++ WpfCoinApp/WpfCoinApp/Models/Coininfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCoinApp.Models
+{
+    internal class Coininfo
+    {
+        public class data
+        {
+            public string id { get; set; }
+            public int rank { get; set; }
+            public string symbol { get; set; }
+            public string name { get; set; }
+            public double supply { get; set; }
+            public double maxSupply { get; set; }
+            public double marketCapUsd { get; set; }
+            public double volumeUsd24Hr { get; set; }
+            public double priceUsd { get; set; }
+            public double changePercent24Hr { get; set; }
+            public double vwap24Hr { get; set; }
+            public DateTime explorer { get; set; }
+
+        }
+
+        public class Root
+        {
+            public data data { get; set; }
+        }
+    }
+}
